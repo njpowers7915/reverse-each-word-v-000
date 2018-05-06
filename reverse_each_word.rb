307c8af0f878890265509022d266734ade3ac054
Nicholas Powers <njpowers7915@gmail.com>
2018-05-06 16:21:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,10 +7,10 @@ def reverse_each_word(sentence)
   x.join(" ")
 end
 
-#def reverse_each_word(sentence)
-#  new_array = sentence.split(" ")
-#  new_array.collect do |word|
-#    word.reverse
-#  end
-#  new_array.join(" ")
-#end
+def reverse_each_word(sentence)
+  new_array = sentence.split(" ")
+  new_array.collect do |word|
+    word.reverse
+  end
+  new_array.join(" ")
+end
